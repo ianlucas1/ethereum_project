80091a331f5edcc576ab228ca3e613e87cf961f8
Ian Lucas <ianlucas1@gmail.com>
2025-05-05 09:40:29 -0700

    docs: add module docstring for src/utils/__init__

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,5 @@
+"""Utility package for ethereum_project, exposing core helpers."""
+
 from src.config import settings
 from .cache import disk_cache
 from .api_helpers import robust_get, _save_api_snapshot
