e61fb2dfd608638d7d92c931fd481778ea8751eb
Ian Lucas <ianlucas1@gmail.com>
2025-05-11 07:57:28 -0700

    feat: add fail_flake8_comment.py for exp 2-5 (comment fail #2)

---
+++ fail_flake8_comment.py
@@ -0,0 +1,6 @@
+# This is a Python file for Experiment 2-5 Prime (attempt 3).
+# It contains a comment line that is deliberately too long to pass flake8 checks (E501). Black should not reformat this comment, so flake8 should fail.
+# This_comment_is_intentionally_made_extremely_long_to_ensure_that_it_violates_the_maximum_line_length_restriction_enforced_by_flake8_specifically_the_E501_error_code_even_after_black_has_had_a_chance_to_reformat_the_rest_of_the_file_as_black_typically_does_not_alter_long_comment_lines_which_is_precisely_the_behavior_we_are_relying_on_for_this_test_case_to_succeed_in_generating_a_CI_failure_condition_for_us_to_observe_and_log.
+
+def short_function():
+    pass 
